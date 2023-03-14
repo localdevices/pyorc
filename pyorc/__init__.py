529262341a987460046bba01dc407f59a3e5cd90
hcwinsemius <h.c.winsemius@gmail.com>
2023-03-14 11:23:37 +0100

    fixing entry points and version bump

--- pyorc/__init__.py
+++ pyorc/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 from .api.cameraconfig import CameraConfig, load_camera_config, get_camera_config
 from .api.video import Video
 from .api.frames import Frames
 from .api.velocimetry import Velocimetry
 from .api.transect import Transect
 from . import service
+from . import cli
