e9de46c10f87ad2c649134bb8d4b467d23cd2420
hcwinsemius <h.c.winsemius@gmail.com>
2023-11-10 15:46:43 +0100

    bumping version and changelog #148

--- pyorc/__init__.py
+++ pyorc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 from .api.cameraconfig import CameraConfig, load_camera_config, get_camera_config
 from .api.video import Video
 from .api.frames import Frames
