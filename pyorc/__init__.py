0a8742164449afaad771d0ac2ec4c2398710949f
hcwinsemius <h.c.winsemius@gmail.com>
2024-06-28 08:57:22 +0200

    bump version

--- pyorc/__init__.py
+++ pyorc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 from .api.cameraconfig import CameraConfig, load_camera_config, get_camera_config
 from .api.video import Video
 from .api.frames import Frames
