2a8c7a3d928bb2e80df8444540a8771ba1fd3ad8
hcwinsemius <h.c.winsemius@gmail.com>
2024-10-30 09:53:49 +0100

    184 ruff precommit and 185 improved transect plot (#185)
    * add ruff tools #184
    
    * ruff formatting, add function to densify cross section points #184
    
    * funcs for plotting wetted cross section #183
    
    * fix #184
    
    * remove ref in docstring _frames_plot #184
    
    * fix #183
    
    * re-added tests for transect plotting
    
    * fix #188 add ruff linting #184

--- tests/test_transect.py
+++ tests/test_transect.py
@@ -1,13 +1,15 @@
-import pytest
 import numpy as np
-import matplotlib.pyplot as plt
+import pytest
+
 
 def test_get_river_flow(piv_transect):
     # fill method is already tested in get_q, so choose default only
     piv_transect.transect.get_q()
     piv_transect.transect.get_river_flow()
     # we allow for 0.001 m3/s deviation for differences in versions of libs
-    assert(np.allclose(piv_transect.river_flow.values, [0.0821733 , 0.08626413, 0.09137767, 0.09649121, 0.10058204], atol=0.001))
+    assert np.allclose(
+        piv_transect.river_flow.values, [0.0821733, 0.08626413, 0.09137767, 0.09649121, 0.10058204], atol=0.001
+    )
 
 
 @pytest.mark.parametrize(
@@ -17,28 +19,30 @@ def test_get_river_flow(piv_transect):
         "log_interp",
         "log_fit",
         "interpolate",
-    ]
+    ],
 )
 def test_get_q(piv_transect, fill_method):
     piv_transect.load()
-    f = piv_transect.transect.get_q(fill_method=fill_method)
+    piv_transect.transect.get_q(fill_method=fill_method)
     # assert if filled values are more complete than non-filled
+
+
+def test_get_wetted_perspective(piv_transect):
+    piv_transect.load()
+    f = piv_transect.transect.get_q()
+    f.transect.get_wetted_perspective(h=0.0)
 
 
 @pytest.mark.parametrize(
     "mode",
-    [
-        "local",
-        "camera",
-        "geographical"
-    ]
+    ["local", "camera", "geographical"],
 )
 @pytest.mark.parametrize(
     "method",
     [
         "quiver",
         # "scatter",
-    ]
+    ],
 )
 def test_plot(piv_transect, mode, method):
     piv_transect.transect.get_q()
