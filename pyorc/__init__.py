7552a8e341e1c20c23823006f70224cc7f969e20
hcwinsemius <h.c.winsemius@gmail.com>
2024-12-10 19:08:33 +0100

    change order of import cli #193

--- pyorc/__init__.py
+++ pyorc/__init__.py
@@ -2,9 +2,9 @@
 
 __version__ = "0.7.0"
 
-from . import cli, service
-from .api import CameraConfig, Frames, Transect, Velocimetry, Video, get_camera_config, load_camera_config
-from .project import *
+from .api import CameraConfig, Frames, Transect, Velocimetry, Video, get_camera_config, load_camera_config  # noqa
+from .project import *  # noqa
+from . import cli, service  # noqa
 
 __all__ = [
     "CameraConfig",
